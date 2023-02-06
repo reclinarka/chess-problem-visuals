d9c827aeb8ce57e68e3c051bd56ba8e141aea951
reclinarka <reclinarka@gmail.com>
2023-02-06 17:56:02 +0100

    Initial commit

---
+++ chess-problem-visuals/chess-visuals.py
@@ -0,0 +1,82 @@
+import xml.etree.ElementTree as ET
+
+try:
+    from ipywidgets import Layout, HTML
+
+    ipy_loaded = True
+except ModuleNotFoundError:
+    print("Jupyter not found, using standard wrapper")
+    ipy_loaded = False
+
+QUEEN_SVG_STRING = """<g id="white-queen" class="white queen" fill="#fff" fill-rule="evenodd" stroke="#000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M8 12a2 2 0 1 1-4 0 2 2 0 1 1 4 0zM24.5 7.5a2 2 0 1 1-4 0 2 2 0 1 1 4 0zM41 12a2 2 0 1 1-4 0 2 2 0 1 1 4 0zM16 8.5a2 2 0 1 1-4 0 2 2 0 1 1 4 0zM33 9a2 2 0 1 1-4 0 2 2 0 1 1 4 0z"/><path d="M9 26c8.5-1.5 21-1.5 27 0l2-12-7 11V11l-5.5 13.5-3-15-3 15-5.5-14V25L7 14l2 12zM9 26c0 2 1.5 2 2.5 4 1 1.5 1 1 .5 3.5-1.5 1-1.5 2.5-1.5 2.5-1.5 1.5.5 2.5.5 2.5 6.5 1 16.5 1 23 0 0 0 1.5-1 0-2.5 0 0 .5-1.5-1-2.5-.5-2.5-.5-2 .5-3.5 1-2 2.5-2 2.5-4-8.5-1.5-18.5-1.5-27 0z" stroke-linecap="butt"/><path d="M11.5 30c3.5-1 18.5-1 22 0M12 33.5c6-1 15-1 21 0" fill="none"/></g>"""  # noqa: E501
+
+
+def _svg(viewbox: int, size: int = None) -> ET.Element:
+    svg = ET.Element("svg", {
+        "xmlns": "http://www.w3.org/2000/svg",
+        "xmlns:xlink": "http://www.w3.org/1999/xlink",
+        "version": "1.2",
+        "baseProfile": "tiny",
+        "viewBox": f"0 0 {viewbox:d} {viewbox:d}",
+    })
+    if size is not None:
+        svg.set("width", str(size))
+        svg.set("height", str(size))
+    return svg
+
+
+class SvgWrapper(str):
+    def _repr_svg_(self):
+        return self
+
+    def save_svg(self, file):
+        with open(file, "w") as f:
+            f.write(self)
+
+
+def _attrs(attrs):
+    return {k: str(v) for k, v in attrs.items() if v is not None}
+
+
+def paint_nxn_board(n: int = 8, Qs: list = None, SQUARE_SIZE: int = 16, size: int = None, ipy_off=False):
+    svg = _svg(n * SQUARE_SIZE, size)
+    fields = [(file, rank) for file in range(n) for rank in range(n)]
+    for file_index, rank_index in fields:
+        x = file_index * SQUARE_SIZE
+        y = rank_index * SQUARE_SIZE
+        light = (file_index + rank_index) % 2 == 0
+        cls = ["square", "light" if light else "dark"]
+        square_color = "#ffce9e" if light else "#d18b47"
+        ET.SubElement(svg, "rect", _attrs({
+            "x": x,
+            "y": y,
+            "width": SQUARE_SIZE,
+            "height": SQUARE_SIZE,
+            "class": " ".join(cls),
+            "stroke": "none",
+            "fill": square_color,
+            "opacity": 1.0,
+        }))
+    if Qs:
+        defs = ET.SubElement(svg, "defs")
+        defs.append(ET.fromstring(QUEEN_SVG_STRING))
+        scale = SQUARE_SIZE / 45
+        if any(q >= n or q < 0 for q in Qs if q is not None): raise ValueError("At leas one queen is in impossible field")
+        assert len(Qs) <= n, "too many queens"
+        for file_index, rank_index in enumerate(Qs):
+            if rank_index is None:
+                continue
+            x = file_index * SQUARE_SIZE
+            y = rank_index * SQUARE_SIZE
+            href = f"#white-queen"
+            ET.SubElement(svg, "use", _attrs({
+                "href": href,
+                "xlink:href": href,
+                "transform": f"translate({x:d}, {y:d}) scale({scale:f})",
+            }))
+    svg = SvgWrapper(ET.tostring(svg).decode("utf-8"))
+    if ipy_loaded and not ipy_off:
+        html_wrapper = f'<div style="width:25%">{svg}</div>'
+        svg_widget = HTML(value=html_wrapper, layout=Layout(grid_area="top"))
+        return svg_widget
+    return svg
